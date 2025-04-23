0d5de25964ee2581419aed31987271125ca68c08
hyunhu24 <140373389+hyunhu24@users.noreply.github.com>
2025-04-24 02:18:02 +0900

    모바일 헤더 서브메뉴

--- src/constants/menuItems.js
+++ src/constants/menuItems.js
@@ -6,18 +6,50 @@ import { MdCollectionsBookmark } from "react-icons/md";
 import { FaUserCircle } from "react-icons/fa";
 import { FaStore } from "react-icons/fa6";
 import { FiLogOut } from "react-icons/fi";
+import { IoIosArrowDown } from "react-icons/io";
 
 export const menuItems = [
+  { name: '메인', icon: <RiDashboardFill/>, path: '/' },
+  { name: '추천메뉴', icon: <MdRecommend /> , path: '/recommend'},
+  { name: '도감', icon: <MdCollectionsBookmark /> , path: '/dictionary'},
+  { name: '마이페이지', icon: <FaUserCircle /> , path: '/mypage'},
+  { name: '상점', icon: <FaStore /> , path: '/shop'},
+  { name: '랭킹', icon: <FaStore /> , path: '/ranking'},
+  // { name: '로그아웃', icon: <FiLogOut /> , path: '/logout'},
+  // { name: '회원가입', icon: <HiUserAdd /> , path: '/signup'},
+  { name: '로그인', icon: <FiLogIn /> , path: '/login'},
+];
+
+export const mobileMenuItems = [
     { name: '메인', icon: <RiDashboardFill/>, path: '/' },
     { name: '추천메뉴', icon: <MdRecommend /> , path: '/recommend'},
     { name: '도감', icon: <MdCollectionsBookmark /> , path: '/dictionary'},
-    { name: '마이페이지', icon: <FaUserCircle /> , path: '/mypage'},
-    { name: '밥풀상점', icon: <FaStore /> , path: '/shop'},
+    { 
+      name: '마이페이지',
+      icon: <FaUserCircle />,
+      arrowIcon: <IoIosArrowDown />,
+      isDropdown: true,
+      subMenus: [
+        { name: '내 게시글', path: '/mypage/' },
+        { name: '도전과제', path: '/mypage/challenges' },
+        { name: '정보', path: '/mypage/info' },
+        { name: '북마크 리스트', path: '/mypage/bookmarks' },
+        { name: '비밀번호 변경', path: '/mypage/change-password' },
+        { name: '회원탈퇴', path: '/mypage/withdrawal' },
+      ]
+    },
+    { name: '상점',
+      icon: <FaStore /> , 
+      arrowIcon: <IoIosArrowDown />,
+      isDropdown: true,
+      subMenus: [
+        { name: '밥풀 상점', path: '/shop/' },
+        { name: '밥풀 내역', path: '/shop/history' },
+      ]
+    },
     { name: '랭킹', icon: <FaStore /> , path: '/ranking'},
-    // { name: '로그아웃', icon: <FiLogOut /> , path: '/logout'},
-    // { name: '회원가입', icon: <HiUserAdd /> , path: '/signup'},
     { name: '로그인', icon: <FiLogIn /> , path: '/login'},
-  ];
+];
 
 export const categoryItems = [
     { name: '전체' },
