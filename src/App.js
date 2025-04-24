99b81ae3a1da0cf1f4062962d480b02528273e92
hyunhu24 <140373389+hyunhu24@users.noreply.github.com>
2025-04-25 02:41:54 +0900

    로딩바 배경 생기는 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Layout from './components/Layout';
 import Main from './pages/Main';
 import MyPage from './pages/MyPage';
@@ -24,41 +25,47 @@ import ChangePassword from './pages/ChangePassword';
 import Withdrawal from './pages/mypage/Withdrawal';
 import Ranking from './pages/Ranking';
 import { ToastProvider } from './hooks/useToast';
+import ProfileChange from './pages/ProfileChange';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <ToastProvider>
-        <DictionaryProvider>
-          <BookmarkProvider>
-            <Layout>
-              <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/mypage" element={<MyPage />}>
-                  <Route index element={<MyPosts />} />
-                  <Route path="create-post" element={<CreatePost />} />
-                  <Route path="challenges" element={<Challenges />} />
-                  <Route path="titles" element={<Titles />} />
-                  <Route path="bookmarks" element={<BookmarkList />} />
-                  <Route path="change-password" element={<ChangePassword />} />
-                  <Route path="withdrawal" element={<Withdrawal />} />
-                </Route>
-                <Route path="/shop" element={<Shop />}>
-                  <Route index element={<ShopMain />} />
-                  <Route path="history" element={<ShopHistory />} />
-                </Route>
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/find-id" element={<FindId />} />
-                <Route path="/find-password" element={<FindPassword />} />
-                <Route path="/recommend" element={<RecommendMenu />} />
-                <Route path="/dictionary" element={<Dictionary />} />
-                <Route path="/ranking" element={<Ranking />} />
-              </Routes>
-            </Layout>
-          </BookmarkProvider>
-        </DictionaryProvider>
-      </ToastProvider>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <ToastProvider>
+          <DictionaryProvider>
+            <BookmarkProvider>
+              <Layout>
+                <Routes>
+                  <Route path="/" element={<Main />} />
+                  <Route path="/mypage" element={<MyPage />}>
+                    <Route index element={<MyPosts />} />
+                    <Route path="create-post" element={<CreatePost />} />
+                    <Route path="challenges" element={<Challenges />} />
+                    <Route path="titles" element={<Titles />} />
+                    <Route path="bookmarks" element={<BookmarkList />} />
+                    <Route path="change-password" element={<ChangePassword />} />
+                    <Route path="withdrawal" element={<Withdrawal />} />
+                  </Route>
+                  <Route path="/shop" element={<Shop />}>
+                    <Route index element={<ShopMain />} />
+                    <Route path="history" element={<ShopHistory />} />
+                  </Route>
+                  <Route path="/signup" element={<SignUp />} />
+                  <Route path="/find-id" element={<FindId />} />
+                  <Route path="/find-password" element={<FindPassword />} />
+                  {/* <Route path="/recommend" element={<RecommendMenu />} /> */}
+                  <Route path="/recommend" element={<ProfileChange />} />
+                  <Route path="/dictionary" element={<Dictionary />} />
+                  <Route path="/ranking" element={<Ranking />} />
+                </Routes>
+              </Layout>
+            </BookmarkProvider>
+          </DictionaryProvider>
+        </ToastProvider>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
