6f7c2893ea2388aa68598852660224fe7385ee95
Hong-min1010 <qza7367@gmail.com>
2025-04-27 20:06:19 +0900

    회원가입 페이지 기능구현중

--- src/api/mutations/userServices.js
+++ src/api/mutations/userServices.js
@@ -1,35 +1,89 @@
 import instance from '../axiosInstance';
 
-export const loginService = async (credentials) => {
-    const loginResponse = await instance.post('/auth/login', credentials);
+// export const loginService = async (credentials) => {
+//     const loginResponse = await instance.post('/auth/login', credentials);
 
-    const accessToken = loginResponse.headers?.authorization?.split(" ")[1] || null;
-    const refreshToken = loginResponse.headers?.refreshtoken || null;
-    const memberId = loginResponse.headers?.memberid || null;
+//     const accessToken = loginResponse.headers?.authorization?.split(" ")[1] || null;
+//     const refreshToken = loginResponse.headers?.refreshtoken || null;
+//     const memberId = loginResponse.headers?.memberid || null;
 
-    if (!accessToken || !refreshToken || !memberId) {
-      throw new Error('로그인 응답에 유효한 정보가 없습니다.');
-    }
+//     console.log("accessToken ======", accessToken);
 
-    localStorage.setItem("refreshToken", refreshToken);
-    localStorage.setItem("memberId", memberId);
+//     if (!accessToken || !refreshToken || !memberId) {
+//       throw new Error('로그인 응답에 유효한 정보가 없습니다.');
+//     }
 
-    const memberResponse = await instance.get(`/members/${memberId}`, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
+//     localStorage.setItem("refreshToken", refreshToken);
+//     localStorage.setItem("memberId", memberId);
 
-    console.log("memberResponse", memberResponse.data.data);
+//     const memberResponse = await instance.get(`/members/${memberId}`, {
+//       headers: { Authorization: `Bearer ${accessToken}` },
+//     });
 
-    if (accessToken) {
-      const tokenWithBearer = accessToken.startsWith("Bearer ")
-        ? accessToken
-        : `Bearer ${accessToken}`;
-      localStorage.setItem("accessToken", tokenWithBearer);
-    }
+//     if (accessToken) {
+//       const tokenWithBearer = accessToken.startsWith("Bearer ")
+//         ? accessToken
+//         : `Bearer ${accessToken}`;
+//       localStorage.setItem("accessToken", tokenWithBearer);
+//     }
   
-    if (memberResponse.data.data) {
-      return { member: memberResponse.data.data, accessToken };
-    } else {
-      throw new Error("사용자 정보를 찾을 수 없습니다.");
-    }
-  };
+//     console.log("memberResponse================", memberResponse.data);
+//     console.log("memberResponse================", memberResponse.data.data);
+
+//     if (memberResponse.data.data) {
+//       return { member: memberResponse.data.data, accessToken };
+//     } else {
+//       throw new Error("사용자 정보를 찾을 수 없습니다.");
+//     }
+//   };
+
+
+export const loginService = async (credentials) => {
+  const loginResponse = await instance.post('/auth/login', credentials);
+
+  const accessToken = loginResponse.headers?.authorization || null;
+  const refreshToken = loginResponse.headers?.refreshtoken || null;
+  const memberId = loginResponse.headers?.memberid || null;
+
+  console.log("accessToken ======", accessToken);
+
+  if (!accessToken || !refreshToken || !memberId) {
+    throw new Error('로그인 응답에 유효한 정보가 없습니다.');
+  }
+
+  localStorage.setItem("accessToken", accessToken);
+  localStorage.setItem("refreshToken", refreshToken);
+  localStorage.setItem("memberId", memberId);
+
+  console.log("accessTokenTest =========", localStorage.getItem("accessToken"));
+
+  const memberResponse = await instance.get(`/members/${memberId}`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+
+
+  console.log("memberResponse.data================", memberResponse.data);
+  console.log("memberResponse.data.data================", memberResponse.data.data);
+
+  if (memberResponse.data.data) {
+    return { member: memberResponse.data.data, accessToken };
+  } else {
+    throw new Error("사용자 정보를 찾을 수 없습니다.");
+  }
+};
+
+// export const signUp = async (memberData) => {
+//   const response = await instance.post("/members",memberData);
+//   return response.data;
+// }
+
+// export const checkDuplicateId = async (loginId) => {
+//   const response = await instance.post("/members/id",{loginId});
+//   console.log("response====",response);
+//   return response;
+// }
+
+// export const checkDuplicateNickName = async (nickName) => {
+//   const response = await instance.post("/members/name", { nickName });
+//   return response;
+// }
