3ab38ddbc616ee2b222aecccdb398702bbf36cd7
hyunhu24 <140373389+hyunhu24@users.noreply.github.com>
2025-05-21 02:51:43 +0900

    마이페이지 프로필 css 수정

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import HeaderPC from './HeaderPC';
-import HeaderMobile from './HeaderMobile';
-import useIsMobile from '../hooks/useIsMobile';
-import RankingSection from './RankingSection';
-import MyPageMenu from './MyPageMenu';
-import ShopMenu from './ShopMenu';
-import CameraColorSelector from './CameraColorSelector';
-import CategoryMenuList from './CategoryMenuList';
-import { useDictionary } from '../contexts/DictionaryContext';
-import { useShowDetail } from '../contexts/ShowDetailContext';
-import CommonProfile from './CommonProfile';
-import { useUser } from '../hooks/useUser';
-import { useSelectedMember } from '../contexts/SelectedMemberContext';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import HeaderPC from "./HeaderPC";
+import HeaderMobile from "./HeaderMobile";
+import useIsMobile from "../hooks/useIsMobile";
+import RankingSection from "./RankingSection";
+import MyPageMenu from "./MyPageMenu";
+import ShopMenu from "./ShopMenu";
+import CameraColorSelector from "./CameraColorSelector";
+import CategoryMenuList from "./CategoryMenuList";
+import { useDictionary } from "../contexts/DictionaryContext";
+import { useShowDetail } from "../contexts/ShowDetailContext";
+import CommonProfile from "./CommonProfile";
+import { useUser } from "../hooks/useUser";
+import { useSelectedMember } from "../contexts/SelectedMemberContext";
 // 레이아웃 컴포넌트 => 왼쪽 오른쪽 섹션 레이아웃 형식
 const Layout = ({ children }) => {
   const { isMobile, isTablet } = useIsMobile();
@@ -23,85 +23,96 @@ const Layout = ({ children }) => {
 
   console.log("member -=-=-=-=-=-=-", member);
 
-  const {
-    activeCategory,
-    setActiveCategory,
-  } = useDictionary();
+  const { activeCategory, setActiveCategory } = useDictionary();
 
   // console.log("showDetail", showDetailContext);
 
   // 현재 경로에 따른 activeMenu 설정
   const getActiveMenu = (path) => {
     // 기본 경로 체크
-    if (path === '/') return '메인';
-    if (path === '/recommend') return '추천메뉴';
-    if (path === '/ranking') return '랭킹';
-    if (path === '/dictionary') return '도감';
-    if (path === '/signup') return '회원가입';
-    if (path === '/login') return '로그인';
+    if (path === "/") return "메인";
+    if (path === "/recommend") return "추천메뉴";
+    if (path === "/ranking") return "랭킹";
+    if (path === "/dictionary") return "도감";
+    if (path === "/signup") return "회원가입";
+    if (path === "/login") return "로그인";
 
     // 마이페이지 경로 체크
-    if (path.startsWith('/mypage')) {
-      return '마이페이지';
+    if (path.startsWith("/mypage")) {
+      return "마이페이지";
     }
 
     // 상점 경로 체크
-    if (path.startsWith('/shop')) {
-      return '상점';
+    if (path.startsWith("/shop")) {
+      return "상점";
     }
 
-    return '';
+    return "";
   };
 
   // 현재 페이지에 따른 사이드 메뉴 결정
   const getSideMenu = () => {
-    if (location.pathname.startsWith('/mypage')) {
+    if (location.pathname.startsWith("/mypage")) {
       return (
         <>
           {!isMobile && (
-           
-            <div className={`flex-shrink-0 h-fit ${isTablet 
-              ? "w-[190px]" 
-              : "w-[360px]"
-            }`}>
-             {member && 
-              <CommonProfile 
-              memberId={member.memberId}
-              memberRanking={false}
-              profileId={member.profileImagePath}
-              nickname={member.nickName}
-              riceCount={member.ricePoint}
-              titleType={member.titles.find(t => t.titleId === member.activeTitleId)?.title?.type}
-              titleImagePath={member.titles.find(t => t.titleId === member.activeTitleId)?.title?.imagePath}
-              titleLevel={member.titles.find(t => t.titleId === member.activeTitleId)?.title?.level}
-              titleName={member.titles.find(t => t.titleId === member.activeTitleId)?.title?.name}
-            />
-              }
-              <div className='bg-white py-6 border border-border '>
-              <MyPageMenu />
+            <div
+              className={`flex-shrink-0 h-fit ${
+                isTablet ? "w-[190px]" : "w-[360px]"
+              }`}>
+              {member && (
+                <CommonProfile
+                  memberId={member.memberId}
+                  memberRanking={false}
+                  profileId={member.profileImagePath}
+                  nickname={member.nickName}
+                  riceCount={member.ricePoint}
+                  titleType={
+                    member.titles.find(
+                      (t) => t.titleId === member.activeTitleId
+                    )?.title?.type
+                  }
+                  titleImagePath={
+                    member.titles.find(
+                      (t) => t.titleId === member.activeTitleId
+                    )?.title?.imagePath
+                  }
+                  titleLevel={
+                    member.titles.find(
+                      (t) => t.titleId === member.activeTitleId
+                    )?.title?.level
+                  }
+                  titleName={
+                    member.titles.find(
+                      (t) => t.titleId === member.activeTitleId
+                    )?.title?.name
+                  }
+                />
+              )}
+              <div className="bg-white pt-6 border border-border mt-6">
+                <MyPageMenu />
               </div>
             </div>
-
           )}
         </>
       );
     }
-    if (location.pathname.startsWith('/shop')) {
+    if (location.pathname.startsWith("/shop")) {
       return (
-        <div className={`bg-white py-6 border border-border flex-shrink-0 h-fit ${isTablet 
-          ? "w-[190px]" 
-          : "w-[360px]"
-        }`}>
+        <div
+          className={`bg-white py-6 border border-border flex-shrink-0 h-fit ${
+            isTablet ? "w-[190px]" : "w-[360px]"
+          }`}>
           <ShopMenu />
         </div>
       );
     }
-    if (location.pathname === '/dictionary') {
+    if (location.pathname === "/dictionary") {
       return (
-        <div className={`bg-white border border-border flex-shrink-0 h-fit ${isTablet 
-          ? "w-[190px]" 
-          : "w-[360px]"
-        }`}>
+        <div
+          className={`bg-white border border-border flex-shrink-0 h-fit ${
+            isTablet ? "w-[190px]" : "w-[360px]"
+          }`}>
           {showDetailContext ? (
             <CategoryMenuList
               onSelect={setActiveCategory}
@@ -113,43 +124,73 @@ const Layout = ({ children }) => {
         </div>
       );
     }
-    if (location.pathname === '/ranking') {
+    if (location.pathname === "/ranking") {
       return (
-        <div className={`bg-white border border-border flex-shrink-0 h-fit ${isTablet 
-          ? "w-[190px]" 
-          : "w-[360px]"
-        }`}>
+        <div
+          className={`bg-white border border-border flex-shrink-0 h-fit ${
+            isTablet ? "w-[190px]" : "w-[360px]"
+          }`}>
           {selectedMember ? (
-            <CommonProfile 
+            <CommonProfile
               memberId={selectedMember.id}
               profileId={selectedMember.profileImagePath}
               nickname={selectedMember.nickName}
               riceCount={selectedMember.ricePoint}
-              titleType={selectedMember.titles.find(t => t.titleId === selectedMember.activeTitleId)?.title?.type}
-              titleImagePath={selectedMember.titles.find(t => t.titleId === selectedMember.activeTitleId)?.title?.imagePath}
-              titleLevel={selectedMember.titles.find(t => t.titleId === selectedMember.activeTitleId)?.title?.level}
-              titleName={selectedMember.titles.find(t => t.titleId === selectedMember.activeTitleId)?.title?.name}
+              titleType={
+                selectedMember.titles.find(
+                  (t) => t.titleId === selectedMember.activeTitleId
+                )?.title?.type
+              }
+              titleImagePath={
+                selectedMember.titles.find(
+                  (t) => t.titleId === selectedMember.activeTitleId
+                )?.title?.imagePath
+              }
+              titleLevel={
+                selectedMember.titles.find(
+                  (t) => t.titleId === selectedMember.activeTitleId
+                )?.title?.level
+              }
+              titleName={
+                selectedMember.titles.find(
+                  (t) => t.titleId === selectedMember.activeTitleId
+                )?.title?.name
+              }
             />
-          ) : member && (
-            <CommonProfile 
-              memberId={member.memberId}
-              profileId={member.profileImagePath}
-              nickname={member.nickName}
-              riceCount={member.ricePoint}
-              titleType={member.titles.find(t => t.titleId === member.activeTitleId)?.title?.type}
-              titleImagePath={member.titles.find(t => t.titleId === member.activeTitleId)?.title?.imagePath}
-              titleLevel={member.titles.find(t => t.titleId === member.activeTitleId)?.title?.level}
-              titleName={member.titles.find(t => t.titleId === member.activeTitleId)?.title?.name}
-            />
+          ) : (
+            member && (
+              <CommonProfile
+                memberId={member.memberId}
+                profileId={member.profileImagePath}
+                nickname={member.nickName}
+                riceCount={member.ricePoint}
+                titleType={
+                  member.titles.find((t) => t.titleId === member.activeTitleId)
+                    ?.title?.type
+                }
+                titleImagePath={
+                  member.titles.find((t) => t.titleId === member.activeTitleId)
+                    ?.title?.imagePath
+                }
+                titleLevel={
+                  member.titles.find((t) => t.titleId === member.activeTitleId)
+                    ?.title?.level
+                }
+                titleName={
+                  member.titles.find((t) => t.titleId === member.activeTitleId)
+                    ?.title?.name
+                }
+              />
+            )
           )}
         </div>
       );
     }
     return (
-      <div className={`bg-white py-6 border border-border flex-shrink-0 h-fit ${isTablet 
-        ? "w-[260px]" 
-        : "w-[360px]"
-      }`}>
+      <div
+        className={`bg-white py-6 border border-border flex-shrink-0 h-fit ${
+          isTablet ? "w-[260px]" : "w-[360px]"
+        }`}>
         <RankingSection />
       </div>
     );
@@ -164,66 +205,61 @@ const Layout = ({ children }) => {
         <HeaderPC activeMenu={getActiveMenu(location.pathname)} />
       )}
 
-      <div className={`
+      <div
+        className={`
         w-full
         flex flex-grow
-        ${isMobile 
-          ? "mt-16 flex-col" 
-          : "max-w-[1920px] mx-auto w-full"
-        }
+        ${isMobile ? "mt-16 flex-col" : "max-w-[1920px] mx-auto w-full"}
       `}>
-        <div className={`
+        <div
+          className={`
           flex w-full h-full p-6 gap-6
-          ${isMobile 
-            ? "flex-col-reverse" 
-            : "flex-row"
-          }
+          ${isMobile ? "flex-col-reverse" : "flex-row"}
         `}>
           {/* 메인 컨텐츠 영역 */}
-          <main className={`
+          <main
+            className={`
             bg-white border border-border p-6
-            ${isMobile 
-              ? "w-full h-full" 
-              : 
-                "flex-1"
-            }
+            ${isMobile ? "w-full h-full" : "flex-1"}
             overflow-y-auto scrollbar-hide
           `}>
             {children}
           </main>
-          {isMobile && 
+          {isMobile && (
             <div className="w-full">
-              {location.pathname === '/dictionary' && (
+              {location.pathname === "/dictionary" && (
                 <>
-                {showDetailContext ? (
-                  <div className="border border-border">
-                    <CategoryMenuList
-                      onSelect={setActiveCategory}
-                      activeCategory={activeCategory}
-                  />
-                  </div>
-                ) : (
-                  <div className="border border-border">
-                    <CameraColorSelector />
-                  </div>
-                )}
+                  {showDetailContext ? (
+                    <div className="border border-border">
+                      <CategoryMenuList
+                        onSelect={setActiveCategory}
+                        activeCategory={activeCategory}
+                      />
+                    </div>
+                  ) : (
+                    <div className="border border-border">
+                      <CameraColorSelector />
+                    </div>
+                  )}
                 </>
               )}
-              {(location.pathname === '/mypage' || location.pathname === '/ranking') && member && (
-                <div className="border border-border">
-                  <CommonProfile 
-                    profileId={member.profileImagePath}
-                    nickname={member.nickName}
-                    riceCount={member.riceCount}
-                    titleType={member.titles[0].title.type}
-                    titleImagePath={member.titles[0].title.imagePath}
-                    titleLevel={member.titles[0].title.level}
-                    titleName={member.titles[0].title.name}
-                  />
-                </div>
-              )}
+              {(location.pathname === "/mypage" ||
+                location.pathname === "/ranking") &&
+                member && (
+                  <div className="border border-border">
+                    <CommonProfile
+                      profileId={member.profileImagePath}
+                      nickname={member.nickName}
+                      riceCount={member.riceCount}
+                      titleType={member.titles[0].title.type}
+                      titleImagePath={member.titles[0].title.imagePath}
+                      titleLevel={member.titles[0].title.level}
+                      titleName={member.titles[0].title.name}
+                    />
+                  </div>
+                )}
             </div>
-          }
+          )}
 
           {/* 오른쪽 박스 */}
           {!isMobile && getSideMenu()}
@@ -233,4 +269,4 @@ const Layout = ({ children }) => {
   );
 };
 
-export default Layout; 
+export default Layout;
