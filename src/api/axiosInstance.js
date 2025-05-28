19e87c69f06cd10a22c098f7aa8bb55344a59467
hyunhu24 <140373389+hyunhu24@users.noreply.github.com>
2025-05-29 06:28:47 +0900

    axios url 변경

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,43 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://ec2-54-180-8-125.ap-northeast-2.compute.amazonaws.com:8080',
-    headers: {
-        'Content-Type': 'application/json',
-        // 'withCredentials': true,
-        // 토큰이 Bearer 앞에 붙은 상태로 localStorage에 저장이 되있어서 뺌
-        'Authorization': `${localStorage.getItem('accessToken')}`
-    },
+  //   baseURL: "http://ec2-54-180-8-125.ap-northeast-2.compute.amazonaws.com:8080",
+  baseURL: "https://api.cookkking.com",
+  headers: {
+    "Content-Type": "application/json",
+    // 'withCredentials': true,
+    // 토큰이 Bearer 앞에 붙은 상태로 localStorage에 저장이 되있어서 뺌
+    Authorization: `${localStorage.getItem("accessToken")}`,
+  },
 });
 
 // 요청 인터셉터
 instance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('accessToken');
-        if (token) {
-            config.headers.Authorization = token;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = token;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 // 응답 인터셉터
 instance.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (error.response?.status === 401) {
-            // 토큰이 만료되었거나 유효하지 않은 경우
-            localStorage.removeItem('accessToken');
-            localStorage.removeItem('memberId');
-            localStorage.removeItem('userInfo');
-        }
-        return Promise.reject(error);
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      // 토큰이 만료되었거나 유효하지 않은 경우
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("memberId");
+      localStorage.removeItem("userInfo");
     }
+    return Promise.reject(error);
+  }
 );
 
 export default instance;
